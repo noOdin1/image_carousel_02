e13ef1ceb16f2aa51947c54895a80c787d1ac3c0
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-11-12 06:27:32 +0800

    Add event listener to both next and prev button, which calls showSlides

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@ let timeInterval = 2000;
 let interval = setInterval(runShow, timeInterval);
 // showSlides(slideIndex);
 
+let btns = document.getElementsByClassName("button");
+Array.from(btns).forEach((item) => {
+  item.addEventListener("click", () => {
+    console.log("Button clicked: " + event.target.id);
+    if (event.target.id == "prevBtn") {
+      showSlides((slideIndex -= 1));
+    }
+    if (event.target.id == "nextBtn") {
+      showSlides((slideIndex += 1));
+    }
+  });
+});
+
